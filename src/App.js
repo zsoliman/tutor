c09e3c1c6aed49742e4608540c9428c15d79e848
Zachary Soliman <zachsoliman1@gmail.com>
2023-05-26 14:42:13 -0400

    navbar links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './components/Home';
+import Learn from './components/Learn';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,11 @@ function App() {
           <Route
             path='/'
             element={<Home />}
+          />
+
+          <Route
+            path='/learn'
+            element={<Learn />}
           />
 
         </Routes>
---
+++ src/components/Learn.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar"
+
+const Learn = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Learning Center</h1>
+        </div>
+    )
+}
+
+export default Learn;
