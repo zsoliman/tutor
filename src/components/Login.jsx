8b01dd56a7463437e9b24e4883210a1a37ff090b
Zachary Soliman <zachsoliman1@gmail.com>
2023-05-29 13:39:02 -0400

    login form with show password function works

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,11 +4,17 @@ const Login = () => {
 
     const [passwordShown, setPasswordShown] = useState(false);
 
+    const togglePassword = (e) => {
+        e.preventDefault()
+        setPasswordShown(!passwordShown)
+    }
+
     return (
         <div className="loginForm">
             <form >
                 <input type="text" name="username" placeholder="username..." />
                 <input type={passwordShown ? "text" : "password"} name="password" placeholder="password..." />
+                <button onClick={togglePassword}>Show Password</button>
                 <input type="submit" value="Log In" />
             </form>
         </div >
